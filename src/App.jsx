bd7e7465de1d2829ee7b3d394de1f37391b91b76
faizamairaj <faizamairaj0@gmail.com>
2025-01-21 13:22:39 +0500

    random-number-task-10

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import './App.css'
+
 function App() {
   const [number, setNumber] = useState(null);
 
@@ -9,9 +10,9 @@ function App() {
   };
 
   return (
-    <div className='bg-pink-500 text-white font-bold text-5xl mt-64 rounded-lg'>
+    <div className='bg-pink-500 text-white font-bold text-4xl mt-64  rounded-lg bg-cover bg-center bg-[url("image.png")] sm:bg-[url("image.png")]'>
       <h1>Random Number Generator</h1>
-      <button className='bg-blue-500 p-5 rounded-md shadow-2xl mt-8 mb-10'  onClick={generateRandomNumber} > Generate Number </button>
+      <button className='bg-blue-500 p-5 rounded-md shadow-2xl mt-8 mb-10' onClick={generateRandomNumber}> Generate Number </button>
       {number !== null && <h2>Generated Number: {number}</h2>}
     </div>
   );
