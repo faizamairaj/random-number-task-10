057aff411285548e9778057c334e121b538e3ac9
faizamairaj <faizamairaj0@gmail.com>
2025-01-21 13:43:27 +0500

    random-number-task-10

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,12 @@ function App() {
   };
 
   return (
-    <div className=' text-white font-bold text-4xl mt-64  rounded-lg bg-cover bg-center bg-[url("image.png")] sm:bg-[url("image.png")]'>
+    <div className='text-white font-bold text-4xl mt-64 rounded-lg bg-cover bg-center' style={{
+      backgroundImage: 'url("image.png")',
+      backgroundRepeat: 'no-repeat',
+      backgroundSize: 'cover',
+    }}>
+    
       <h1>Random Number Generator</h1>
       <button className='bg-blue-500 p-5 rounded-md shadow-2xl mt-8 mb-10' onClick={generateRandomNumber}> Generate Number </button>
       {number !== null && <h2>Generated Number: {number}</h2>}
